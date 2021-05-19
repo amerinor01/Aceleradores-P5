782738fab52d4285922738325483ca83950e9123
Alberto Merino <Alberto.Merino@uclm.es>
2021-05-19 12:19:00 +0200

    end the script func

--- script.sh
+++ script.sh
@@ -3,21 +3,64 @@
 DOUBLE_PROGRAM="./double.out"
 FLOAT_PROGRAM="./float.out"
 
+FLOAT_FILE=test\/float.txt
+FLOAT_SEQ=test\/float_seq.txt
+DOUBLE_FILE=test\/double.txt
+DOUBLE_SEQ=test\/double_seq.txt
+TEMP_FILE=test\/temp.txt
+
 N=(1024 2048 4096 8192) # Add more if we use Pekin
+EVENTS_FLAGS="warps_launched,local_store,global_store,local_load,global_load,shared_load,shared_store"
 
-for i in ${N[@]}
-do
-	for j in {0..5}
+METRICS_FLAGS="system_utilization,shared_load_transactions_per_request,shared_store_transactions_per_request,\
+local_load_transactions_per_request,local_store_transactions_per_request,\
+shared_store_transactions,shared_load_transactions,local_load_transactions,\
+local_store_transactions,gld_transactions,gst_transactions"
+
+NVPROF=$(echo "sudo nvprof --log-file " $TEMP_FILE  "-m " \
+$METRICS_FLAGS "-e " $EVENTS_FLAGS)
+
+
+# Check if we are sudoers
+if [[ "sudo" -eq  0 ]]
+then
+	# Iterate for every matrix size
+	for i in ${N[@]}
 	do
-		for k in {1..10}
+		# For every program
+		for j in {0..4}
 		do
-			echo "It:" $k "Option:" $j "N:" $i >> test\/float.txt
-			$FLOAT_PROGRAM $j $i $i 0  >> test\/float.txt
-			echo "" >> test\/float.txt
+			# Ten times every program
+			for k in {1..10}
+			do
+				echo "It:" $k "Option:" $j "N:" $i >> $FLOAT_FILE
+				$NVPROF $FLOAT_PROGRAM $j $i $i 0  >> $FLOAT_FILE
+				#echo $NVPROF $FLOAT_PROGRAM $j $i $i 0
+				sed -n 9,15p $TEMP_FILE | sed 's/  */ /g' >> $FLOAT_FILE
+				sed -n 21,30p $TEMP_FILE | sed 's/  */ /g' >> $FLOAT_FILE
+				echo "" >> $FLOAT_FILE
 
-			echo "It:" $k "Option:" $j "N:" $i >> test\/double.txt
-			$DOUBLE_PROGRAM $j $i $i 0 >> test\/double.txt
-			echo "" >> test\/double.txt
+				echo "It:" $k "Option:" $j "N:" $i >> $DOUBLE_FILE
+				$NVPROF $DOUBLE_PROGRAM $j $i $i 0 >> $DOUBLE_FILE
+				sed -n 9,15p $TEMP_FILE | sed 's/  */ /g' >> $DOUBLE_FILE
+				sed -n 21,30p $TEMP_FILE | sed 's/  */ /g' >> $DOUBLE_FILE
+				echo "" >> $DOUBLE_FILE
+			done
 		done
+			#Seq code doesnt generate NVPROF data so it's separate
+			for k in {1..10}
+			do
+				echo "It:" $k "Option: 5 N:" $i >> $FLOAT_SEQ
+				$FLOAT_PROGRAM 5  $i $i 0  >> $FLOAT_SEQ
+				echo "" >> $FLOAT_SEQ
+
+				echo "It:" $k "Option: 5 N:" $i >> $DOUBLE_SEQ
+				$DOUBLE_PROGRAM 5  $i $i 0  >> $DOUBLE_SEQ
+				echo "" >> $DOUBLE_SEQ
+			done
 	done
-done
+else
+	echo "exec with root privilegies"
+fi
+
+exit 0
